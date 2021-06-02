71a647e33773e3a7dc1b7bbae1bfcaf8e091c67d
Dale Furneaux <dfurneau@lab1r1s12.42abudhabi.ae>
2021-06-02 21:36:17 +0400

    Khalifa added test cases

--- ex00.c
+++ ex00.c
@@ -20,12 +20,37 @@ void *timer(void *var)
 void *execute(void *var)
 {
 	var = 0x00;
-	char *string = "Hu4kf7$";
 	char *str;
 
 	printf("\nQuote: Would it save you a lot of time if I just gave up and went mad now?\n\n");
-	str = ft_strdup(string);
-	printf("string = %s | ret %s", string, str);
+	
+	char *string1 = "Pa2mDTow2O0rcWF9QFipJAb7kpkFdNjmvdfKQ6uL";
+	str = ft_strdup(string1);
+	printf("string = %s\t| ret %s\n", string1, str);
+	free(str);
+	char *string2 = "ffJykmhacQ2rn3Nm1";
+	str = ft_strdup(string2);
+	printf("string = %s\t\t\t\t| ret %s\n", string2, str);
+	free(str);
+	char *string3 = "sXlEDtdEkD";
+	str = ft_strdup(string3);
+	printf("string = %s\t\t\t\t\t| ret %s\n", string3, str);
+	free(str);
+	char *string4 = "usFWKEz3sNyIi49fz6G";
+	str = ft_strdup(string4);
+	printf("string = %s\t\t\t\t| ret %s\n", string4, str);
+	free(str);
+	char *string5 = "5PmuKqUFZXAPHI6A3mZ3Rj";
+	str = ft_strdup(string5);
+	printf("string = %s\t\t\t\t| ret %s\n", string5, str);
+	free(str);
+	char *string6 = "FplJKDPP6KZCGhlaMdjr6j4YTd6iNh";
+	str = ft_strdup(string6);
+	printf("string = %s\t\t\t| ret %s\n", string6, str);
+	free(str);
+	char *string7 = "mbewt2H8xImgsxzQULr3a3cG0sYTeBsGaZsZqYJ9ps";
+	str = ft_strdup(string7);
+	printf("string = %s\t| ret %s\n", string7, str);
 	free(str);
 
 	func_done = 1;
